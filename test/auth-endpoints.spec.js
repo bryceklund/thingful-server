1dab8234bfd390ceedd35b16b01f170d2d0712ec
Bryce Eklund <bryce.eklund@gmail.com>
2019-08-04 15:09:20 -0700

    refactor to jwt auth

---
+++ test/auth-endpoints.spec.js
@@ -0,0 +1,9 @@
+const knex = require('knex')
+const app = require('../src/app')
+const helpers = require('./test-helpers')
+
+describe.only('Auth Endpoints', () => {
+    let db
+
+    const { testUsers } = helpers.ma
+})
